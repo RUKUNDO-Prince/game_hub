add32d6cdaa0c18a092eaea278f395e3d83fcfb1
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-09-28 21:48:36 +0300

    get optimised images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,11 +12,10 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
-          {/* {console.log(game.metacritic)} */}
           <PlatformIconList platforms={game.platforms.map(p => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
