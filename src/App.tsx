22f626c0f3f1cd99d3f7896f4ed212caa84ddafd
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-09-29 11:58:49 +0300

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,21 @@ import GenreList from './components/GenreList'
 
 const App = () => {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}>
+    <Grid 
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr"
+      }}
+    >
       <GridItem area="nav">
         <Navbar />
       </GridItem>
       <Show above='lg'>
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,26 @@
-import { Genre } from '../hooks/useGenres'
-import useData from '../hooks/useData';
+import { Genre } from "../hooks/useGenres";
+import useData from "../hooks/useData";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const { data } = useData<Genre>('/genres');
+  const { data } = useData<Genre>("/genres");
   return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
-  )
-}
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default GenreList
+export default GenreList;
