eb584b484be4925b66cdb7aad41b997eb8d206fc
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-20 21:55:40 +0200

    game-hub

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = () => {
     fetchNextPage,
     hasNextPage,
   } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   if (error) return <Text>{error.message}</Text>;
 
